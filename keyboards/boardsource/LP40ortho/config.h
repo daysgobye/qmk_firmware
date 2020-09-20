c776f33b215fb6a7d64346c5e8dda608e77999a6
Cole Smith <smithcole@protonmail.com>
2020-09-20 00:42:41 -0700

    got basics of rgb matrix in

--- keyboards/boardsource/LP40ortho/config.h
+++ keyboards/boardsource/LP40ortho/config.h
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      LP40-O
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
 
 /*
  * Keyboard Matrix Assignments
@@ -40,21 +40,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS {D0, D1, D2, D3}
+#define MATRIX_COL_PINS {D4, D5, D6, B0, B1, B2, B3, B4, B5, B6, B7, C7}
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-/*
- * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
- */
-#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
-
 //#define BACKLIGHT_PIN B7
 //#define BACKLIGHT_LEVELS 3
 //#define BACKLIGHT_BREATHING
+#define RGBLIGHT_ANIMATIONS
+#define RGB_DI_PIN C6
+
+#ifdef RGBLIGHT_ENABLE
+#define RGBLED_NUM 12    // Number of LEDs
+#endif
+
+#define DRIVER_LED_TOTAL  54
+
 
 //#define RGB_DI_PIN E2
 //#ifdef RGB_DI_PIN
